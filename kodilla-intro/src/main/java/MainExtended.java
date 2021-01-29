f55b7f4e13fd17486a6f60b4d4bd21b58229e0e8
Wojciech Głowa <wojciechrajdom@gmail.com>
2021-01-29 13:15:58 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Wojciech";
+        int myAge = 36;
+        double height = 1.83;
+        char carClass = 'G';
+        int weight = 94;
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(weight);
+        System.out.println(isGoodProgrammer);
+    }
+}
+
