5e16eba10dec5952233a7a887d32622990edf425
Wojciech Głowa <wojciechrajdom@gmail.com>
2021-01-29 12:13:35 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        String str = "Wojciech";
+        int i = 36;
+        char c = 'g';
+    }
+}
