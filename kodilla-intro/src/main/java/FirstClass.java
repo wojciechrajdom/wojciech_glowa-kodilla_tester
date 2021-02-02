dca684de98214c80fac518c21136629ec9d258bd
Wojciech Głowa <wojciechrajdom@gmail.com>
2021-02-02 07:38:11 +0100

    SimpleArray

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,11 @@
-public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        String example = "Hello from FirstClass!";
-        System.out.println(example); // [3]
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+
+        if (1 > 2) {
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
     }
 }
---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,20 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[5];
+
+        animals[0] = "dog";
+        animals[1] = "cat";
+        animals[2] = "horse";
+        animals[3] = "bird";
+        animals[4] = "tiger";
+
+        String animal = animals[2];
+
+        int numberOfElements = animals.length;
+
+        System.out.println("My table has 5 elements");
+    }
+}
+
+
+
